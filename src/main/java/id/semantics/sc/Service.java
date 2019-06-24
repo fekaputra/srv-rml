40fe15c3655f69e6abf4ec980f0e284e2d7f76fc
Fajar <fajar.ekaputra@tuwien.ac.at>
2019-06-24 16:20:10 +0200

    cors

--- src/main/java/id/semantics/sc/Service.java
+++ src/main/java/id/semantics/sc/Service.java
@@ -151,6 +151,23 @@ public class Service {
         // set port
         port(2806);
 
+        // set options
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         // status checks
         // -- no additional parameters required
         get("/api/sparql/status", (request, response) -> {
